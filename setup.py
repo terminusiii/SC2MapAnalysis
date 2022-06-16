22e5fbc37bfc202715a5b1b6be4903f8c693184d
Max Sandholm <msandhol@gmail.com>
2022-06-16 22:11:46 +0300

    Update numpy version to match ladder

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 requirements = [  # pragma: no cover
         "wheel",
-        "numpy~=1.19.2",
+        "numpy~=1.22.3",
         "Cython",
         "burnysc2",
         "matplotlib",
@@ -32,9 +32,9 @@ requirements = [  # pragma: no cover
 setup(  # pragma: no cover
         name="sc2mapanalyzer",
         # version=f"{__version__}",
-        version="0.0.87",
+        version="0.0.88",
         install_requires=requirements,
-        setup_requires=["wheel", "numpy~=1.19.2"],
+        setup_requires=["wheel", "numpy~=1.22.3"],
         cmdclass={"build_ext": build_ext},
         ext_modules=[mapping_module],
         packages=["MapAnalyzer", "MapAnalyzer.cext"],
