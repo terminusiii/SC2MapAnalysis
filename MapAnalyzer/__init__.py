593cb2b643279a23f2123baa4b2b180362781811
Tom Kerr <tomkerrdev@gmail.com>
2023-09-05 11:13:04 +0100

    style: format imports

--- MapAnalyzer/__init__.py
+++ MapAnalyzer/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "0.0.87"
 
 # flake8: noqa
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .constructs import ChokeArea, MDRamp, VisionBlockerArea
 from .MapData import MapData
 from .Polygon import Polygon
 from .Region import Region
-from .constructs import ChokeArea, MDRamp, VisionBlockerArea
-from pkg_resources import get_distribution, DistributionNotFound
